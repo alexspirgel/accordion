95724a92f5d56d20f6c0eeea03f0f06611ccde71
Alexander Spirgel <spirgel.alexander@gmail.com>
2018-10-23 08:09:26 -0400

    Updated naming in gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 const gulp = require('gulp');
-const gulp_sass = require('gulp-sass');
+const gulpSass = require('gulp-sass');
 const webpack = require('webpack');
-const webpack_stream = require('webpack-stream');
+const webpackStream = require('webpack-stream');
 const webpack_config = require('./webpack.config.js');
 
 gulp.task('css', () => {
 	return gulp.src('./src/css/*.scss')
-		.pipe(gulp_sass().on('error', gulp_sass.logError))
+		.pipe(gulpSass().on('error', gulpSass.logError))
 		.pipe(gulp.dest('./dist/css'));
 });
 
 gulp.task('js', () => {
 	gulp.src('./src/js/accordion.js')
-		.pipe(webpack_stream(webpack_config), webpack)
+		.pipe(webpackStream(webpack_config), webpack)
 		.pipe(gulp.dest('./dist/js'));
 });
 
