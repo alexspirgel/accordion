ca1e9b170ed69809d249d23f5f687e388fcd4b42
Alexander Spirgel <spirgel.alexander@gmail.com>
2018-12-24 15:33:31 -0500

    Added error handling for webpack

--- gulpfile.js
+++ gulpfile.js
@@ -13,10 +13,13 @@ gulp.task('css', () => {
 gulp.task('js', () => {
 	gulp.src('./src/js/ace-accordion.js')
 		.pipe(webpackStream(webpack_config), webpack)
+		.on('error', function() {
+			this.emit('end');
+		})
 		.pipe(gulp.dest('./dist/js'));
 });
 
-gulp.task('watch', function() {
+gulp.task('watch', () => {
 	gulp.watch('./src/css/*.scss', ['css']);
 	gulp.watch('./src/js/*.js', ['js']);
 });
