f0c7d28860f7431891fd3d861c3b0bbb70075615
Alexander Spirgel <spirgel.alexander@gmail.com>
2019-01-02 15:12:33 -0500

    Begin setting information on heading element

--- src/js/heading.js
+++ src/js/heading.js
@@ -25,6 +25,15 @@ const Heading = class {
 		// Add the heading element reference.
 		this.element = heading_element;
 
+		// Set the heading role attribute to heading.
+		heading_element.setAttribute('role', 'heading');
+		// Set the heading aria-controls attribute to the item content id.
+		heading_element.setAttribute('aria-controls', this.parent_instance.content.element.id);
+		// Set the accordion item heading aria-level attribute.
+		// accordion_item_heading.setAttribute('aria-level', accordion_level);
+		// Set the accordion item heading aria-expanded attribute.
+		// accordion_item_heading.setAttribute('aria-expanded', aria_expanded_value);
+
 		// Return this instance.
 		return this;
 
